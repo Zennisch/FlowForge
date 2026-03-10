bea3cb217b1e5452d2707ad662a3e41c082b7c48
Zennisch <zennisch@gmail.com>
2026-03-11 03:18:37 +0900

    create: install typescript and database configuration

--- FlowForge-Server/src/app.module.ts
+++ FlowForge-Server/src/app.module.ts
@@ -1,1 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AppConfigModule } from './infra/config/config.module';
+import { DatabaseModule } from './infra/database/mongoose.provider';
+
+@Module({
+  imports: [AppConfigModule, DatabaseModule],
+})
+export class AppModule {}
 
--- FlowForge-Server/src/infra/database/mongoose.provider.ts
+++ FlowForge-Server/src/infra/database/mongoose.provider.ts
@@ -1,1 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('MONGODB_URI'),
+      }),
+    }),
+  ],
+  exports: [MongooseModule],
+})
+export class DatabaseModule {}
 
