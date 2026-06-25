17341c9d519b2b27a7ae852f4ccc72e33643d58a
Thiên Phú <zennisch@gmail.com>
2026-06-26 01:57:16 +0900

    update: log for server

--- FlowForge-Server/src/main.ts
+++ FlowForge-Server/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import morgan = require('morgan');
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use(
+      morgan(
+        ':date[iso] :remote-addr :method :url :status :response-time ms :res[content-length] - :user-agent',
+      ),
+    );
+  }
 
   const allowedOrigins = (process.env.CORS_ORIGINS ?? '')
     .split(';')
